343a49c8afebf2cb3f000e63f9a5a8fbb985494d
Anisha-20 <anishadatta61@gmail.com>
2025-01-07 14:28:28 +0530

    Adding and subtraction

---
+++ add_sub.py
@@ -0,0 +1,17 @@
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+
+sw = input("You want add or sub?: ")
+
+def addition(a, b):
+    sum = a + b
+    print(sum)
+
+def subtraction(a, b):
+    sub = a - b
+    print(sub)
+
+if sw=="add":
+    addition(a, b)
+else
+    subtraction(a, b)
